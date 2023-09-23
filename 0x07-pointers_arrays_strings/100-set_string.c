8a908b399e588f62ca37ef213cf6ac30159dc918
Zethembe <nsithole032@gmail.com>
2023-09-22 20:14:48 -0700

    More projects on pointers, arrays and strings in C

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * set_string - sets pointer to char
+ * @s: ptr to ptr
+ * @to: ptr to char
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
+
---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_diagsums - Sum Diagonals
+ * @a: input
+ * @size: input
+ * Return: 0 Always (Success)
+ */
+void print_diagsums(int *a, int size)
+{
+	int diag1_sum, diag2_sum, diag;
+
+	diag1_sum = 0;
+	diag2_sum = 0;
+        diag = 0;
+
+	while (diag < size)
+	{
+		diag1_sum += a[diag * size + diag];
+		diag++;
+	}
+
+	for (diag = size - 1; diag >= 0; diag--)
+	{
+		diag2_sum = diag2_sum a[diag * size + (size - diag - 1)];
+	}
+
+	printf("%d, %d\n", diag1_sum, diag2_sum);
+}
