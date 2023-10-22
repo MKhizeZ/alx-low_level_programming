dc54802e6601518283e238d574aa451f121a1b43
Zethembe <nsithole032@gmail.com>
2023-10-22 12:50:52 -0700

    Updated 0-print_list.c task

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,25 +9,17 @@
  */
 size_t print_list(const list_t *h)
 {
+	size_t size_init = 0;
 
-    size_t Node_iter;
-    Node_iter = 0;
+	while (h)
+	{
+		if (!h->str)
+			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", h->len, h->str);
+		h = h->next;
+		size_init++;
+	}
 
-    while (head)
-    {
-        if (head->str)
-        {
-            printf("[%u] %s\n", head->len, head->str);
-        }
-        else
-        {
-            printf("[0] (nil)\n");
-        }
-        
-	/*moves along nodes*/
-        head = head->next;
-        Node_iter++; /*increments node count*/
-    }
-
-    return Node_iter;
+	return (size_init);
 }
