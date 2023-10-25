d862fcf24e7539c3a3981d476ef011017dcbbc0b
Zethembe <nsithole032@gmail.com>
2023-10-25 03:03:45 -0700

    Corrected 4-free_list.c

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -7,14 +7,14 @@
  */
 void free_list(list_t *head)
 {
-    list_t *current = head;  // Pointer to keep track of the current node being processed.
-    list_t *next;  // Pointer to store the next node in the list.
+	list_t *current = head;  /* Pointer to track the current node*/
+	list_t *next;  /* Pointer to store the next node in the list.*/
 
-    while (current != NULL)
-    {
-        next = current->next;  // Store the next node before freeing the current node.
-        free(current->str);  // Free the memory allocated for the string in the current node.
-        free(current);  // Free the memory allocated for the current node.
-        current = next;  // Move to the next node in the list.
-    }
+	while (current != NULL)
+	{
+		next = current->next;  /* Store the next node before freeing*/
+		free(current->str);  /* Free memory allocated for the string*/
+		free(current);  /* Free the memory allocated for the current node.*/
+		current = next;  /* Move to the next node in the list.*/
+	}
 }
