2d1c2a4de7110e06ab2d704de88bbf9833ed67ef
Zethembe <nsithole032@gmail.com>
2023-10-25 03:13:32 -0700

    Correction to 100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,20 +1,22 @@
 #include <stdio.h>
 
 /**
- * @brief Constructor function that is automatically called before the main function.
+ * __attribute__ - Constructor function that is automatically called before
+ * the main function.
  *
- * Marked with the `__attribute__ ((constructor))` attribute, which ensures that it is automatically
+ * Marked with the `__attribute__ ((constructor))` attribute,
+ * which ensures that it is automatically
  * called before the `main` function is executed.
  */
 void __attribute__ ((constructor)) pre_main(void);
 
 /**
- * @brief Prints a message to the console before main
+ * pre_main - Prints a message to the console before main
  *
  * This function is called by the constructor function 'pre_main'
  */
 void pre_main(void)
 {
-    printf("You're beat! and yet, you must allow,\n"
-           "I bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n"
+			"I bore my house upon my back!\n");
 }
