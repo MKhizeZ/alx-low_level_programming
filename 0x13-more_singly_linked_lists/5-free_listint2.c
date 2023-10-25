a512e429aa23984429d790e77bac84f4644aa12e
Zethembe <nsithole032@gmail.com>
2023-10-25 04:12:53 -0700

    Corrected 5-free_listint2.c task

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -6,13 +6,13 @@
  */
 void free_listint2(listint_t **head)
 {
+	listint_t *current = *head;
+	listint_t *next;
+
 	if (head == NULL)
 	{
 		return;
 	}
-
-	listint_t *current = *head;
-	listint_t *next;
 
 	while (current != NULL)
 	{
